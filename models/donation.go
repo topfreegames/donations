8a80592ff20950b9421c8d8616e971f0363b4b74
Bernardo Heynemann <heynemann@gmail.com>
2016-11-16 17:41:17 -0200

    Donation models

---
+++ models/donation.go
@@ -0,0 +1,119 @@
+package models
+
+//go:generate easyjson -no_std_marshalers $GOFILE
+
+import (
+	"fmt"
+
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
+	"github.com/topfreegames/donations/log"
+	"github.com/uber-go/zap"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//Donation represents a specific donation a player made to a request
+//easyjson:json
+type Donation struct {
+	Player string `json:"player" bson:"player"`
+	Amount int    `json:"amount" bson:"amount"`
+}
+
+//DonationRequest represents a request for an item donation a player made in a game
+//easyjson:json
+type DonationRequest struct {
+	ID        string     `json:"id" bson:"_id,omitempty"`
+	Item      string     `json:"item" bson:"item,omitempty"`
+	Player    string     `json:"player" bson:"player"`
+	Clan      string     `json:"clan" bson:"clan"`
+	Game      *Game      `json:"game" bson:"game"`
+	Donations []Donation `json:"donations" bson:""`
+}
+
+//Create new donation request
+func (d *DonationRequest) Create(db *mgo.Database, logger zap.Logger) error {
+	l := logger.With(
+		zap.String("source", "DonationRequestModel"),
+		zap.String("operation", "Save"),
+	)
+
+	d.ID = bson.NewObjectId().String()
+
+	log.D(l, "Saving donation request...")
+	err := GetDonationRequestsCollection(db).Insert(d)
+
+	if err != nil {
+		log.E(l, "Failed to save donation request.", func(cm log.CM) {
+			cm.Write(zap.Error(err))
+		})
+		return err
+	}
+
+	log.D(l, "Donation Request saved successfully.")
+
+	return nil
+}
+
+//Donate an item in a given Donation Request
+func (d *DonationRequest) Donate(db *mgo.Database, player string, amount int, logger zap.Logger) error {
+	l := logger.With(
+		zap.String("source", "DonationRequestModel"),
+		zap.String("operation", "Donate"),
+		zap.String("player", player),
+		zap.Int("amount", amount),
+	)
+
+	if d.ID == "" {
+		return fmt.Errorf("Can't donate without a proper DB-loaded DonationRequest (ID is required).")
+	}
+
+	log.D(l, "Saving donation...")
+	query := bson.M{"_id": d.ID}
+	update := bson.M{"$push": bson.M{"donations": bson.M{
+		"player": player,
+		"amount": amount,
+	}}}
+
+	err := GetDonationRequestsCollection(db).Update(query, update)
+	if err != nil {
+		log.E(l, "Failed to add donation.", func(cm log.CM) {
+			cm.Write(zap.Error(err))
+		})
+		return err
+	}
+
+	log.D(l, "Donation added successfully.")
+
+	return nil
+}
+
+//ToJSON returns the game as JSON
+func (d *DonationRequest) ToJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	d.MarshalEasyJSON(&w)
+	return w.BuildBytes()
+}
+
+//GetDonationRequestFromJSON unmarshals the game from the specified JSON
+func GetDonationRequestFromJSON(data []byte) (*DonationRequest, error) {
+	donationRequest := &DonationRequest{}
+	l := jlexer.Lexer{Data: data}
+	donationRequest.UnmarshalEasyJSON(&l)
+	return donationRequest, l.Error()
+}
+
+//NewDonationRequest returns a new instance of DonationRequest
+func NewDonationRequest(game *Game, item, player, clan string) *DonationRequest {
+	return &DonationRequest{
+		Game:   game,
+		Item:   item,
+		Player: player,
+		Clan:   clan,
+	}
+}
+
+//GetDonationRequestsCollection to update or query games
+func GetDonationRequestsCollection(db *mgo.Database) *mgo.Collection {
+	return db.C("requests")
+}
---
+++ models/donation_test.go
@@ -0,0 +1,184 @@
+package models_test
+
+import (
+	mgo "gopkg.in/mgo.v2"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	uuid "github.com/satori/go.uuid"
+	"github.com/topfreegames/donations/models"
+	. "github.com/topfreegames/donations/testing"
+	"github.com/uber-go/zap"
+)
+
+var _ = Describe("Donation Model", func() {
+	var logger zap.Logger
+	var session *mgo.Session
+	var db *mgo.Database
+
+	BeforeEach(func() {
+		logger = zap.New(
+			zap.NewJSONEncoder(zap.NoTime()), // drop timestamps in tests
+			zap.FatalLevel,
+		)
+
+		session, db = GetTestMongoDB()
+	})
+	AfterEach(func() {
+		session.Close()
+		session = nil
+		db = nil
+	})
+
+	Describe("Creating a donation request", func() {
+		Describe("Feature", func() {
+			It("Should create a new donation request", func() {
+				game := models.NewGame(
+					uuid.NewV4().String(),
+					uuid.NewV4().String(),
+					map[string]interface{}{"x": 1},
+				)
+				err := game.Save(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				itemID := uuid.NewV4().String()
+				playerID := uuid.NewV4().String()
+				clanID := uuid.NewV4().String()
+				donationRequest := models.NewDonationRequest(
+					game,
+					itemID,
+					playerID,
+					clanID,
+				)
+				err = donationRequest.Create(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				var dbDonationRequest *models.DonationRequest
+				c := models.GetDonationRequestsCollection(db)
+				err = c.FindId(donationRequest.ID).One(&dbDonationRequest)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(dbDonationRequest.Item).To(Equal(itemID))
+				Expect(dbDonationRequest.Player).To(Equal(playerID))
+				Expect(dbDonationRequest.Clan).To(Equal(clanID))
+				Expect(dbDonationRequest.Game).NotTo(BeNil())
+				Expect(dbDonationRequest.Game.ID).To(Equal(game.ID))
+			})
+		})
+
+		Describe("Measure", func() {
+			var game *models.Game
+			BeforeOnce(func() {
+				game = models.NewGame(
+					uuid.NewV4().String(),
+					uuid.NewV4().String(),
+					map[string]interface{}{
+						"a": 1,
+					},
+				)
+				err := game.Save(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			Measure("it should create donation requests fast", func(b Benchmarker) {
+				runtime := b.Time("runtime", func() {
+					itemID := uuid.NewV4().String()
+					playerID := uuid.NewV4().String()
+					clanID := uuid.NewV4().String()
+					donationRequest := models.NewDonationRequest(
+						game,
+						itemID,
+						playerID,
+						clanID,
+					)
+					err := donationRequest.Create(db, logger)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				Expect(runtime.Seconds()).Should(BeNumerically("<", 0.05), "Operation shouldn't take this long.")
+			}, 500)
+		})
+	})
+
+	Describe("Donating an item", func() {
+		Describe("Feature", func() {
+			It("Should append a new donation", func() {
+				game := models.NewGame(
+					uuid.NewV4().String(),
+					uuid.NewV4().String(),
+					map[string]interface{}{"x": 1},
+				)
+				err := game.Save(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				itemID := uuid.NewV4().String()
+				playerID := uuid.NewV4().String()
+				clanID := uuid.NewV4().String()
+				donationRequest := models.NewDonationRequest(
+					game,
+					itemID,
+					playerID,
+					clanID,
+				)
+				err = donationRequest.Create(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				donerID := uuid.NewV4().String()
+				err = donationRequest.Donate(db, donerID, 1, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				var dbDonationRequest *models.DonationRequest
+				c := models.GetDonationRequestsCollection(db)
+				err = c.FindId(donationRequest.ID).One(&dbDonationRequest)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(dbDonationRequest.Donations).To(HaveLen(1))
+
+				Expect(dbDonationRequest.Donations[0].Player).To(Equal(donerID))
+				Expect(dbDonationRequest.Donations[0].Amount).To(Equal(1))
+			})
+		})
+
+		Describe("Measure", func() {
+			var game *models.Game
+			var donationRequest *models.DonationRequest
+
+			BeforeOnce(func() {
+				game = models.NewGame(
+					uuid.NewV4().String(),
+					uuid.NewV4().String(),
+					map[string]interface{}{
+						"a": 1,
+					},
+				)
+				err := game.Save(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+
+				itemID := uuid.NewV4().String()
+				playerID := uuid.NewV4().String()
+				clanID := uuid.NewV4().String()
+				donationRequest = models.NewDonationRequest(
+					game,
+					itemID,
+					playerID,
+					clanID,
+				)
+				err = donationRequest.Create(db, logger)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			Measure("it should donate items fast", func(b Benchmarker) {
+				runtime := b.Time("runtime", func() {
+					donorID := uuid.NewV4().String()
+					err := donationRequest.Donate(
+						db,
+						donorID,
+						1,
+						logger,
+					)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				Expect(runtime.Seconds()).Should(BeNumerically("<", 0.05), "Operation shouldn't take this long.")
+			}, 500)
+		})
+	})
+})
